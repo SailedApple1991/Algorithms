52fc5b9dedb79f9fdaa25128163fb2150ac2623b
unknown <cwang46@buffalo.edu>
2020-12-08 04:06:48 -0500

    daily

---
+++ Stack/946.validate-stack-sequences(stack).java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=946 lang=java
+ *
+ * [946] Validate Stack Sequences
+ *
+ * https://leetcode.com/problems/validate-stack-sequences/description/
+ *
+ * algorithms
+ * Medium (63.08%)
+ * Likes:    1183
+ * Dislikes: 32
+ * Total Accepted:    70.4K
+ * Total Submissions: 111.3K
+ * Testcase Example:  '[1,2,3,4,5]\n[4,5,3,2,1]'
+ *
+ * Given two sequences pushed and popped with distinct values, return true if
+ * and only if this could have been the result of a sequence of push and pop
+ * operations on an initially empty stack.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * Output: true
+ * Explanation: We might do the following sequence:
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * Output: false
+ * Explanation: 1 cannot be popped before 2.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= pushed.length == popped.length <= 1000
+ * 0 <= pushed[i], popped[i] < 1000
+ * pushed is a permutation of popped.
+ * pushed and popped have distinct values.
+ * 
+ * 
+ */
+
+import java.util.*;// @lc code=start
+class Solution {
+    //stack
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        for(int i = 0; i < pushed.length; i++){
+            stack.push(pushed[i]);
+            while(index < popped.length && !stack.isEmpty() && stack.peek() == popped[index]){
+                stack.pop();
+                index++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
+
